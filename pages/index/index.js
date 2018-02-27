21ac634af35a61cb3192f1d922805aa4c029a784
horseee <www.maxinyin@qq.com>
2018-02-27 14:45:28 +0800

    finish the UI for the community

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,13 @@ const app = getApp()
 
 Page({
   data: {
+    PostImage: ["/assets/image/1.jpg", "/assets/image/2.jpg","/assets/image/3.jpg"],
     focus:[],
+    dateYear:"2017",
+    dateMonth:"Jan",
+    dateDay:"01",
+    isShow: true,
+    currentTab: 0,
     /*motto: '你好，小程序',
     userInfo: {},
     hasUserInfo: false,
@@ -15,6 +21,34 @@ Page({
     duration: 1000,
     circular: true,
     pageNumber: 1,
+    upStatus: 0,
+  },
+
+  swichNav: function (e) {
+    if (this.data.currentTab === e.target.dataset.current) {
+      return false;
+    } else {
+      var showMode = e.target.dataset.current == 0;
+      this.setData({
+        currentTab: e.target.dataset.current,
+        isShow: showMode
+      })
+    }
+  },
+
+  onUpTap: function() {
+    this.animationUp.scale(2).step();
+    this.setData({
+      animationUp: this.animationUp.export(),
+      upStatus: 1-this.data.upStatus
+    })
+    setTimeout(function () {
+      this.animationUp.scale(1).step();
+      this.setData({
+        animationUp: this.animationUp.export()
+      })
+    }.bind(this), 300);
+    
   },
 
   onShareAppMessage: function(){
@@ -42,32 +76,45 @@ Page({
     })
   },
 
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-
   onLoad: function () {
     var that = this;
     console.log("加载时触发")
+
+    var animationUp = wx.createAnimation({
+      timingFunction: 'ease-in-out'
+    })
+    this.animationUp = animationUp
+
     wx.request({
       url: 'https://news-at.zhihu.com/api/4/news/latest',
       success: function(e){
-        console.log(e.data.stories);
+        console.log(e.data);
+        var PostDay = e.data.date;
+        var month;
+        switch (e.data.date.slice(4,6)) {
+          case "01": month = "Jan"; break;
+          case "02": month = "Tue"; break;
+          case "03": month = "Mar"; break;
+          case "04": month = "Apr"; break;
+          case "05": month = "May"; break;
+          case "06": month = "June"; break;
+          case "07": month = "July"; break;
+          case "08": month = "Aug"; break;
+          case "09": month = "Sept"; break;
+          case "10": month = "Oct"; break;
+          case "11": month = "Nov"; break;
+          case "12": month = "Dec"; break;
+          default:
+            month = "Jan"
+        }
         that.setData({
-          focus: e.data.stories
+          focus: e.data.stories,
+          dateYear: PostDay.slice(0,4),
+          dateMonth: month,
+          dataDay: PostDay.slice(6,8),
         })
       }
     })
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
+  
 })
--- pages/question/question.js
+++ pages/question/question.js
@@ -1,66 +1,56 @@
-// pages/question/question.js
+var app = getApp()
+
+var interval;
+var varName;
+var ctx = wx.createCanvasContext('canvasArcCir');
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
+  
   data: {
-  
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+  drawCircle: function () {
+    clearInterval(varName);
+    function drawArc(s, e) {
+      ctx.setFillStyle('white');
+      ctx.clearRect(0, 0, 200, 200);
+      ctx.draw();
+      var x = 100, y = 100, radius = 96;
+      ctx.setLineWidth(5);
+      ctx.setStrokeStyle('#d81e06');
+      ctx.setLineCap('round');
+      ctx.beginPath();
+      ctx.arc(x, y, radius, s, e, false);
+      ctx.stroke()
+      ctx.draw()
+    }
+    var step = 1, startAngle = 1.5 * Math.PI, endAngle = 0;
+    var animation_interval = 1000, n = 60;
+    var animation = function () {
+      if (step <= n) {
+        endAngle = step * 2 * Math.PI / n + 1.5 * Math.PI;
+        drawArc(startAngle, endAngle);
+        step++;
+      } else {
+        clearInterval(varName);
+      }
+    };
+    varName = setInterval(animation, animation_interval);
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
   onReady: function () {
-  
+    //创建并返回绘图上下文context对象。
+    var cxt_arc = wx.createCanvasContext('canvasCircle');
+    cxt_arc.setLineWidth(6);
+    cxt_arc.setStrokeStyle('#eaeaea');
+    cxt_arc.setLineCap('round');
+    cxt_arc.beginPath();
+    cxt_arc.arc(100, 100, 96, 0, 2 * Math.PI, false);
+    cxt_arc.stroke();
+    cxt_arc.draw();
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
+  onLoad: function (options) {
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
   }
 })
